f7cebf335bc971b4af23191a7a7c888d8b37867b
NikWay7 <goncarovnikita9@gmail.com>
2022-11-24 17:34:26 +0500

    7

--- Kino.py
+++ Kino.py
@@ -3,9 +3,10 @@ import time
 from selenium.webdriver.common.by import By
 import undetected_chromedriver.v2 as uc
 
-a = input() #выбрать мини-игру
-if a == 1: #2 мини-игра
-    class one : 
+#a = input() #выбрать мини-игру
+#if a == 1: #2 мини-игра
+class one : # создать потом 2 класс
+    
         driver = uc.Chrome(executable_path="chromedriver.exe")
         driver.get("https://www.kinopoisk.ru/special/birthday19/")    
         time.sleep(3)
@@ -16,46 +17,42 @@ if a == 1: #2 мини-игра
         element = driver.find_element(By.XPATH,'/html/body/div[1]/div/section[1]/div/div/div/div[2]/div/div[2]/div[2]/div/button').click() #игра 2
         time.sleep(10)
         element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[2]/div[2]/div[1]/div/div/button').click() #начать
-        time.sleep(10)
-    
+        time.sleep(5)
+         
+        s = []
+        #s2 = []
+        for i in range (2,0,-1):
+           element = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/div[1]/div[1]/div/div[1]")
+           while element.get_attribute('class') != "game__test-life-item game__test-life-item_dead":
+                with open('output.txt') as file: #получаем список исходя из файла с текстом(потом туда будут добавлятся элементы вследствие чего и список будет увеличиваться)
+                    for line in file:
+                        s.append(line)
 
-        #element = driver.find_element(By.CLASS_NAME, 'game__test-question').text
-        #print(element)
-        for i in range (20,0,-1):
-            if i == 10:
-                time.sleep(100)
+                text = driver.find_element(By.CLASS_NAME, 'game__test-question').text
+                for n in range (len(s),0,-1):
+                    if text == s[n]:
+                        if driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/span').text == 
+
+                MyFile = open ('output.txt', 'w')
+                #MyFile2 = open ('output2.txt', 'w')
+                s = map (lambda x: x + '\n', s)
+                #s2 = map (lambda x: x + '\n', s2)  
+                MyFile.writelines(s)
+                #MyFile2.writelines(s2) 
+                MyFile.close ()
+                #MyFile2.close ()
+                if element.get_attribute('class') == "game__test-life-item game__test-life-item_dead":
+                   element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[3]/div[2]/div/div/div/div[3]/button[1]').click()
+                else:
+                    print(9)
 
         time.sleep(15)
-
-
         driver.quit()
-
-        #game__test-life-item - life
-        #game__test-life-item game__test-life-item_dead - dead
-elif  a == 2:
-    s = []
-    #s2 = []
-    with open('output.txt') as file: #получаем список исходя из файла с текстом(потом туда будут добавлятся элементы вследствие чего и список будет увеличиваться)
-        for line in file:
-            s.append(line) 
-         
-
-    MyFile = open ('output.txt', 'w')
-    #MyFile2 = open ('output2.txt', 'w')
-    s = map (lambda x: x + '\n', s)
-    #s2 = map (lambda x: x + '\n', s2)  
-    MyFile.writelines(s)
-    #MyFile2.writelines(s2) 
-    MyFile.close ()
-    #MyFile2.close ()
-   
-print (*s)
-
-
-#/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[1]/div/span 1
-#/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div/span 2
-#/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[3]/div/span 3
-#/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[4]/div/span 4
-
-
+#elif a == 2:
+#class two:
+'''    
 #list.append(x)	Добавляет элемент в конец списка 
+        #element = driver.find_element(By.CLASS_NAME, 'game__test-question').text
+        #print(element)
+        #element.get_attribute('class') возвращает указанный элемент (Элемент)
+'''
