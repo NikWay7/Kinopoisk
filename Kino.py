b892fc3cac413519e0382b30f672b655d34e85cd
NikWay7 <goncarovnikita9@gmail.com>
2022-11-25 02:58:38 +0500

    9

--- Kino.py
+++ Kino.py
@@ -1,4 +1,3 @@
-from msilib.schema import Class
 import time
 from selenium.webdriver.common.by import By
 import undetected_chromedriver.v2 as uc
@@ -20,32 +19,41 @@ class one : # создать потом 2 класс
          
         s = []
         s2 = []
+        
         for i in range (5,0,-1):
-           element = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/div[1]/div[1]/div/div[1]") # last heal
-           while element.get_attribute('class') != "game__test-life-item game__test-life-item_dead":
-                with open('output.txt') as file: #получаем список исходя из файла с текстом(потом туда будут добавлятся элементы вследствие чего и список будет увеличиваться)
-                    for line in file:
-                        s.append(line)
-
-                text = driver.find_element(By.CLASS_NAME, 'game__test-question').text
-                for n in range (len(s),0,-1):
-                    if text == s[n]:
-                        driver.find_element(By.LINK_TEXT, s2[n]).click()
-                    else:
-                        s.append(text)
-                        MyFile = open ('output.txt', 'w')
-                        s = map (lambda x: x + '\n', s)
-                        MyFile.writelines(text)
-                        MyFile.close()
-                        MyFile2 = open ('output2.txt', 'w')
-                        s2 = map (lambda x: x + '\n', s2)
-                        otvet = driver.find_element(By.CLASS_NAME, 'modal-wrong-answer__title').text()
-                        MyFile2.writelines(((otvet[otvet.find("«") + 1:]).split('»')[0]))
-                        MyFile2.close()
-                element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div/span').click()        
-                if element.get_attribute('class') == "game__test-life-item game__test-life-item_dead":
-                   element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[3]/div[2]/div/div/div/div[3]/button[1]').click()
-        time.sleep(10)
+                
+                element1 = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/div[1]/div[1]/div/div[1]") # last heal
+                
+                while element1.get_attribute('class') != "game__test-life-item game__test-life-item_dead":
+                        text = driver.find_element(By.CLASS_NAME, 'game__test-question').text #вопрос
+                        for n in range (len(s),0,-1):####object of type 'map' has no len()
+                                if text == s[n-1]: 
+                                        driver.find_element(By.LINK_TEXT, s2[n]).click()
+                        else:
+                                s.append(text)
+                                MyFile = open ('output.txt', 'a')
+                                s = map (lambda x: x + '\n', s)
+                                MyFile.writelines(text + '\n')
+                                MyFile.close()
+                                element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div/span').click()
+                                if driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/div[1]/div[1]/div/div[3]").get_attribute('class') == "game__test-life-item": #or  driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/div[1]/div[1]/div/div[2]").get_attribute('class') == "game__test-life-item":
+                                        MyFile2 = open ('output2.txt', 'a')
+                                        s2 = map (lambda x: x + '\n', s2)
+                                        otvet = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div/span').text 
+                                        MyFile2.writelines(otvet + '\n')
+                                        MyFile2.close()
+                                else:        
+                                        MyFile2 = open ('output2.txt', 'a')
+                                        s2 = map (lambda x: x + '\n', s2)
+                                        otvet = driver.find_element(By.CLASS_NAME, 'modal-wrong-answer__title').text 
+                                        MyFile2.writelines(((otvet[otvet.find("«") + 1:]).split('»')[0]) + '\n')
+                                        MyFile2.close()
+                                        time.sleep(2)
+                                        element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[3]/div[2]/div/div/div/button').click()
+                        time.sleep(3)      
+                        if element1.get_attribute('class') == "game__test-life-item game__test-life-item_dead":
+                                element = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div/div[3]/div[2]/div/div/div/div[3]/button[1]').click()
+        time.sleep(4)
 
         #driver.quit()
 #elif a == 2:
